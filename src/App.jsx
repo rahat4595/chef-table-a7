fe3494779fe5b400230dc851f3dbedc134fe0b06
Mohammad Rahat <mdrahat4595001@gmail.com>
2024-03-15 23:57:28 +0600

    fixed banner section

--- src/App.jsx
+++ src/App.jsx
@@ -38,10 +38,13 @@ function App() {
     <div className="hero max-w-7xl m-auto h-[600px] rounded-2xl" style={{ backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-          <button className="btn btn-primary">Get Started</button>
+        <div className="w-4/5">
+          <h1 className="mb-5 text-5xl text-white font-bold ">Discover an exceptional cooking class tailored for you!</h1>
+          <p className="mb-5 ">Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding problems to become an exceptionally well world-class Programmer</p>
+          <div className='flex gap-10 justify-center'>
+          <button className="btn bg-[#0BE58A] text-sm rounded-full px-8 border-none">Explore Now</button>
+          <button className="btn bg-transparent text-white text-sm rounded-full px-8 ">Explore Now</button>
+          </div>
         </div>
       </div>
     </div>
