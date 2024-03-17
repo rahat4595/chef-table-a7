d68a5517e8bb878bc659a93f073d0b582f6ea4bf
Mohammad Rahat <mdrahat4595001@gmail.com>
2024-03-17 23:18:52 +0600

    fixed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -70,7 +70,7 @@ function App() {
   </div>
   <div className="flex-none gap-2">
     <div className="form-control hidden lg:block">
-    <label className="input input-bordered flex items-center gap-2">
+    <label className="input input-bordered rounded-full flex items-center gap-2">
    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
     <input type="text" className="grow" placeholder="Search" />
   </label>
@@ -85,6 +85,7 @@ function App() {
   </div>
       </div>
      </nav>
+
     <main className='p-5 md:p-0'>
     <div>
     <div className="hero max-w-7xl m-auto h-[600px] rounded-2xl md:mt-10" style={{ backgroundImage: "url('https://i.ibb.co/S0JsSxd/banner.png')"}}>
