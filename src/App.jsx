dd2b6d692f1e0916b40f379c2f4ec9734aa25e59
Mohammad Rahat <mdrahat4595001@gmail.com>
2024-03-18 12:17:55 +0600

    fixed small issues

--- src/App.jsx
+++ src/App.jsx
@@ -86,43 +86,7 @@ function App() {
       </div>
      </nav>
 
-    {/* <div className="navbar bg-base-100 max-w-7xl m-auto mt-8 flex justify-around">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-          </div>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>Home</a></li>
-            <li>
-              <a>Recipes</a>
-            </li>
-            <li><a>About</a></li>
-            <li><a>Search</a></li>
-          </ul>
-        </div>
-        <a className="btn btn-ghost text-xl lg:text-3xl font-bold">Flavorful Fusions</a>
-      </div>
-      <div className="navbar-start hidden lg:flex mr-28 text-[#150B2BB3] font-normal">
-        <ul className="menu menu-horizontal text-base">
-          <li><a>Home</a></li>
-          <li>
-          <a>Recipes</a>
-          </li>
-          <li><a>About</a></li>
-          <li><a>Search</a></li>
-        </ul>
-      </div>
-
-      <div className='space-x-4'>
-      <label className="input hidden lg:flex items-center gap-2 rounded-3xl bg-[#150B2B0D]">
-        <p className='text-2xl'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg></p>
-      
-  <input type="text" className="grow font-normal text-[#150B2BB3] text-lg" placeholder="Search" />
-  </label>
-     <img className='bg-[#0BE58A] rounded-full' src="https://i.ibb.co/pX03SVM/Frame.png" alt="" />
-      </div>
-    </div> */}
+    
 
     <main className='p-5 md:p-0'>
     <div>
